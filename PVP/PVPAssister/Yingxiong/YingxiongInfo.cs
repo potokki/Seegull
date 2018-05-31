7bfe9fd8ec2dc72f15d164939830c7d6eb56f02d
potokki <pokki.hokki@gmail.com>
2018-05-31 14:50:16 -0500

    Change output column order

--- PVP/PVPAssister/Yingxiong/YingxiongInfo.cs
+++ PVP/PVPAssister/Yingxiong/YingxiongInfo.cs
@@ -8,11 +8,11 @@ namespace PVPAssister.Yingxiong
         public AttributeDependency AttributeDependency = new AttributeDependency();
         public MingwenUnit[] Mingwens;
         public MingwenUnit[] MingwensBySystem;
-        public const string OutputTitle = "Yingxiong,4-类型,4-B,4-G,4-R,5-类型,5-B,5-G,5-R,4-类型,4-B,4-G,4-R,5-类型,5-B,5-G,5-R,D4-B,D4-G,D4-R,D5-B,D5-G,D5-R";
+        public const string OutputTitle = "Yingxiong,4-类型,4-B,4-G,4-R,5-类型,5-B,5-G,5-R,D4-B,D4-G,D4-R,D5-B,D5-G,D5-R,4-类型,4-B,4-G,4-R,5-类型,5-B,5-G,5-R";
 
         public YingxiongInfo()
         {
-            int max = MingwenOverall.MaxMingwenLevel + 1;
+            const int max = MingwenOverall.MaxMingwenLevel + 1;
             Mingwens = new MingwenUnit[max];
             MingwensBySystem = new MingwenUnit[max];
             for (int i = 1; i < max; i++)
@@ -25,7 +25,7 @@ namespace PVPAssister.Yingxiong
         public override string ToString()
         {
             string str =
-                $"{Name},{MingwensBySystem[4]},{MingwensBySystem[5]},{Mingwens[4]},{Mingwens[5]},{Mingwens[4].ToDetailedString()},{Mingwens[5].ToDetailedString()}";
+                $"{Name},{Mingwens[4]},{Mingwens[5]},{Mingwens[4].ToDetailedString()},{Mingwens[5].ToDetailedString()},{MingwensBySystem[4]},{MingwensBySystem[5]}";
 
             return str;
         }
